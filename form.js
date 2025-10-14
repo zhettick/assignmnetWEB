9a1373108f0da5e4d7388ff74716bbeb96e36884
zhettick <belikeotall@gmail.com>
2025-10-14 21:56:17 +0500

    assignment5

---
+++ form.js
@@ -0,0 +1,95 @@
+const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+  const question = item.querySelector(".faq-question");
+  question.addEventListener("click", () => {
+    item.classList.toggle("open");
+  });
+});
+
+const button = document.getElementById("open-popup");
+const popup = document.getElementById("popup");
+const closeBtn = document.querySelector(".close-btn");
+
+button.addEventListener("click", () => {
+  popup.style.display = "flex";
+});
+
+closeBtn.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
+const form = document.querySelector("form.form-custom");
+
+function setErr(id, msg) {
+  document.getElementById(id).textContent = msg;
+}
+
+form.addEventListener("submit", (e) => {
+  setErr("err-name", "");
+  setErr("err-surname", "");
+  setErr("err-email", "");
+  setErr("err-catName", "");
+  setErr("err-catAge", "");
+  setErr("err-gender", "");
+  setErr("err-activities", "");
+
+  let check = true;
+
+  const name = document.getElementById("name").value.trim();
+  const surname = document.getElementById("surname").value.trim();
+  const email = document.getElementById("email").value.trim();
+  const catName = document.getElementById("catName").value.trim();
+  const catAge = document.getElementById("catAge").value.trim();
+  const gender = document.querySelector("input[name='gender']:checked");
+  const activities = [
+    document.getElementById("play").checked,
+    document.getElementById("sleep").checked,
+    document.getElementById("walk").checked
+  ];
+
+  if (name === "") {
+    setErr("err-name", "Please enter your name.");
+    check = false;
+  }
+
+  if (surname === "") {
+    setErr("err-surname", "Please enter your surname.");
+    check = false;
+  }
+
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (email === "" || !emailPattern.test(email)) {
+    setErr("err-email", "Please enter a valid email address.");
+    check = false;
+  }
+
+  if (catName === "") {
+    setErr("err-catName", "Please enter name for your cat.");
+    check = false;
+  }
+
+  if (catAge <= 0 || catAge === "") {
+    setErr("err-catAge", "Please enter your cats age.");
+    check = false;
+  }
+
+  if (!gender) {
+    setErr("err-gender", "Please select cat gender.");
+    check = false;
+  }
+
+  if (!activities.some(v => v)) {
+    setErr("err-activities", "Please choose at least one activity.");
+    check = false;
+  }
+
+  if (!check) {
+    e.preventDefault();
+    return;
+  }
+
+  e.preventDefault();
+  alert("Form submitted successfully!");
+  form.reset();
+});
