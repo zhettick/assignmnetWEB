115b3f5e72c0666b9b1b140288bc05ed0be9cd2a
Zumrad777 <sherbadalovaz@gmail.com>
2025-10-14 21:05:26 +0500

    Add files via upload

--- script1.js
+++ script1.js
@@ -7,12 +7,10 @@ function showKitty(action, img, text) {
 
 function feed() {
   showKitty("feed", "cat/eating.gif", "😺 Your kitty is eating!");
-  
 }
 
 function sleep() {
   showKitty("sleep", "cat/working.gif", "💤 Your kitty is sleeping...");
-  
 }
 
 function dance() { 
@@ -24,11 +22,52 @@ function resetKitty() {
   kitty.style.display = "none"; 
   kitty.src = "";               
   document.getElementById("status").innerText = "Your kitty is waiting...";
-  window.location.href = "care.html"
 }
 
 function logout() {
   alert("You have been logged out!");
   window.location.href = "index.html";
 }
+
+document.getElementById('openSidebar').addEventListener('click', () => {
+  document.getElementById('sidebar').style.display = 'flex';
+});
+
+document.getElementById('closeSidebar').addEventListener('click', () => {
+  document.getElementById('sidebar').style.display = 'none';
+});
+
+   
+
+  
+    const sidebar = document.getElementById('sidebar');
+    document.getElementById('openSidebar').addEventListener('click', () => {
+      sidebar.style.right = '0';
+    });
+    document.getElementById('closeSidebar').addEventListener('click', () => {
+      sidebar.style.right = '-250px';
+    });
+    
+    function changeColor() {
+    const colors = ['#fffaf5', '#ffcbcb', '#ffd6a5', '#caffbf', '#9bf6ff', '#bdb2ff'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    document.body.style.backgroundColor = randomColor;
+  } 
+  
+   function updateDateTime() {
+    const now = new Date();
+    const options = { 
+      weekday: 'short', 
+      year: 'numeric', 
+      month: 'long', 
+      day: 'numeric', 
+      hour: '2-digit', 
+      minute: '2-digit'
+    };
+    document.getElementById("datetime").textContent = now.toLocaleDateString('en-US', options);
+  }
+
+  
+  updateDateTime();
+  setInterval(updateDateTime, 60000);
 
